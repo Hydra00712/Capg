f294120a9b0f2e44114fd471a0c0fb6589e21c4f
Mohamed Adam Benaddi <email@gmail.com>
2026-09-16 22:28:34 +0100

    test commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 2;
+        double result = (double)x/y;
+        System.out.println(result);
+
+
+        String name = "Adam";
+        short age = 22;
+        float height = 1.94f;
+        boolean didGraduate = true;
+        System.out.printf("My name is %s and my age is %d and my height is %f and i have Graduated(%b)",name,age,height,didGraduate);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+    }
+}
